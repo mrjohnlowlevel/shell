101d5803fefc003fe2a1b797d0a2210172b207f6
mrjohnlowlevel <johnfree751@gmail.com>
2025-06-21 19:55:30 +0800

    added header file for piping

---
+++ pipe/pipef.h
@@ -0,0 +1,11 @@
+//
+// Created by julia on 6/21/25.
+//
+
+#ifndef PIPEF_H
+#define PIPEF_H
+
+void exec_piped(char **command1, char **command2);
+int check_pipe(char **tokens);
+
+#endif //PIPEF_H
